7a1a2dc836d80989f31d9e2cde978905e22971e4
frosthoar <197470064+frosthoar@users.noreply.github.com>
2025-03-31 00:16:24 +0200

    refactor

--- tests/test_sfv_file.py
+++ tests/test_sfv_file.py
@@ -3,6 +3,7 @@ import pathlib
 import sys
 
 import hoarder
+import compare_files
 
 test_file_path = pathlib.Path(__file__).parent.resolve()
 add_path = (test_file_path / ".." / "src").resolve()
@@ -10,51 +11,9 @@ sys.path.append(add_path.as_posix())
 
 logger = logging.getLogger("hoarder.test_sfv_file")
 
-sfv_file_compare = [
-    hoarder.hash_file.FileEntry(
-        pathlib.Path("empty_file"),
-        0,
-        False,
-        b"\x00\x00\x00\x00",
-        hoarder.hash_file.Algo.CRC32,
-    ),
-    hoarder.hash_file.FileEntry(  # 902F433D 902F433D
-        pathlib.Path("english text.txt"),
-        637,
-        False,
-        b"\x90\x2f\x43\x3d",
-        hoarder.hash_file.Algo.CRC32,
-    ),
-    hoarder.hash_file.FileEntry(
-        pathlib.Path("Stück.txt"),
-        964,
-        False,
-        b"\xb3\xa6\x5a\xf4",
-        hoarder.hash_file.Algo.CRC32,
-    ),
-    hoarder.hash_file.FileEntry(
-        path=pathlib.Path("sample_dir/english text 2.txt"),
-        size=360,
-        is_dir=False,
-        hash_value=b"\xdf\x9d\x9dH",
-        algo=hoarder.hash_file.Algo.CRC32,
-    ),
-    hoarder.hash_file.FileEntry(
-        pathlib.Path("trailing whitespace .txt"),
-        729,
-        False,
-        b"\x47\x5f\x91\x8b",
-        hoarder.hash_file.Algo.CRC32,
-    ),
-    hoarder.hash_file.FileEntry(
-        pathlib.Path("日本語テキスト.txt"),
-        5070,
-        False,
-        b"\x80\xed\x75\x5f",
-        hoarder.hash_file.Algo.CRC32,
-    ),
+compare_files_wo_dir = [
+    el for el in compare_files.compare_files if not el.is_dir
 ]
-
 
 def test_sfv_files():
     sfv_file_path = (
@@ -62,7 +21,7 @@ def test_sfv_files():
     ).resolve()
     sfv_file = hoarder.SfvFile.from_path(sfv_file_path)
     assert len(sfv_file.files) == 6
-    assert sorted(sfv_file.files) == sorted(sfv_file_compare)
+    assert sorted(sfv_file.files) == sorted(compare_files_wo_dir)
     assert sfv_file.path == sfv_file_path
 
     sfv_file_lowercase_path = (
@@ -72,5 +31,5 @@ def test_sfv_files():
     sfv_file_lowercase = hoarder.SfvFile.from_path(sfv_file_lowercase_path)
 
     assert len(sfv_file_lowercase.files) == 6
-    assert sorted(sfv_file_lowercase.files) == sorted(sfv_file_compare)
+    assert sorted(sfv_file_lowercase.files) == sorted(compare_files_wo_dir)
     assert sfv_file_lowercase.path == sfv_file_lowercase_path
