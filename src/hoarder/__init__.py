a639d39739047e465ca24345d7b6fe89c7d36596
frosthoar <197470064+frosthoar@users.noreply.github.com>
2025-03-31 02:00:21 +0200

    linting

--- src/hoarder/__init__.py
+++ src/hoarder/__init__.py
@@ -1,7 +1,7 @@
 from . import hash_file, rar_file, sfv_file, shared
+from .hash_name_file import HashNameFile
 from .rar_file import RarFile
 from .sfv_file import SfvFile
-from .hash_name_file import HashNameFile
 
 __all__ = [
     "shared",
