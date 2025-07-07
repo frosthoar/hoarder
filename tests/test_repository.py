d838668e179a08157c32fdf7f4748482b9cc59c4
frosthoar <197470064+frosthoar@users.noreply.github.com>
2025-07-08 00:45:11 +0200

    linting

--- tests/test_repository.py
+++ tests/test_repository.py
@@ -1,38 +1,45 @@
-import hoarder
-import tempfile
-import pathlib
 import logging
+import pathlib
+import tempfile
+
+import hoarder
 
 logger = logging.getLogger()
 
+
 def test_repository():
-
     with tempfile.NamedTemporaryFile(mode="w+b", suffix=".sqlite", delete=False) as f:
-            f.close()
-            try:
-                ha_repo = hoarder.HashArchiveRepository(f.name)
+        f.close()
+        try:
+            ha_repo = hoarder.HashArchiveRepository(f.name)
+            saved_sfv_file = hoarder.SfvArchive.from_path(
+                pathlib.Path("./test_files/sfv/rhash_output_lowercase.sfv")
+            )
+            saved_sfv_file_path = saved_sfv_file.path
+            ha_repo.save(saved_sfv_file)
+            retrieved_sfv_file = ha_repo.load(saved_sfv_file_path)
 
-                saved_sfv_file = hoarder.SfvArchive.from_path(pathlib.Path("./test_files/sfv/rhash_output_lowercase.sfv"))
-                saved_sfv_file_path = saved_sfv_file.path
-                ha_repo.save(saved_sfv_file)
-                retrieved_sfv_file = ha_repo.load(saved_sfv_file_path)
+            assert repr(saved_sfv_file) == repr(retrieved_sfv_file)
 
-                assert repr(saved_sfv_file) == repr(retrieved_sfv_file)
+            saved_rar_file = hoarder.RarArchive.from_path(
+                pathlib.Path("./test_files/rar/winrar_rar4_password.rar"),
+                password="password",
+            )
+            saved_rar_file_path = saved_rar_file.path
+            ha_repo.save(saved_rar_file)
+            retrieved_rar_file = ha_repo.load(saved_rar_file_path)
 
-                saved_rar_file = hoarder.RarArchive.from_path(pathlib.Path("./test_files/rar/winrar_rar4_password.rar"), password="password")
-                saved_rar_file_path = saved_rar_file.path
-                ha_repo.save(saved_rar_file)
-                retrieved_rar_file = ha_repo.load(saved_rar_file_path)
+            assert repr(saved_rar_file) == repr(retrieved_rar_file)
 
-                assert repr(saved_rar_file) == repr(retrieved_rar_file)
+            saved_hnf_file = hoarder.HashNameArchive.from_path(
+                pathlib.Path(
+                    "./test_files/hnf/[Foobar] Lowercase and Parens - 11 (x264-AC3)(d74b7612).mkv"
+                )
+            )
+            saved_hnf_file_path = saved_hnf_file.path
+            ha_repo.save(saved_hnf_file)
+            retrieved_hnf_file = ha_repo.load(saved_hnf_file_path)
 
-                saved_hnf_file = hoarder.HashNameArchive.from_path(pathlib.Path("./test_files/hnf/[Foobar] Lowercase and Parens - 11 (x264-AC3)(d74b7612).mkv"))
-                saved_hnf_file_path = saved_hnf_file.path
-                ha_repo.save(saved_hnf_file)
-                retrieved_hnf_file = ha_repo.load(saved_hnf_file_path)
-
-                assert repr(saved_hnf_file) == repr(retrieved_hnf_file)
-            finally:
-                pathlib.Path(f.name).unlink
-
-
+            assert repr(saved_hnf_file) == repr(retrieved_hnf_file)
+        finally:
+            pathlib.Path(f.name).unlink
