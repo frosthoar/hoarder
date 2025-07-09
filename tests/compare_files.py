0fdc5ab2ab0b53891543c54df961cb3ece3dc390
frosthoar <197470064+frosthoar@users.noreply.github.com>
2025-07-09 19:30:58 +0200

    stub

--- tests/compare_files.py
+++ tests/compare_files.py
@@ -14,18 +14,14 @@ def CRC32_from_file(filename: str | pathlib.Path) -> bytes:
         buf = (binascii.crc32(buf) & 0xFFFFFFFF)
         return buf.to_bytes(4, byteorder='big')
 
-dummy_dir_entries = []
-
-test_file_path = ap / ".." / "test_files" / "files"
-
-for entry in os.walk(test_file_path):
+def create_test_file_entry(parent_path, entry)
     path = pathlib.PurePath(entry)
-    size = filestats = os.stat(test_file_path / path).st_size
-    is_dir = (test_file_path / entry).is_dir()
+    size = filestats = os.stat(parent_path / path).st_size
+    is_dir = (parent_path / entry).is_dir()
     if is_dir:
         hash = bytes(4)
     else:
-        hash = CRC32_from_file(test_file_path / entry)
+        hash = CRC32_from_file(parent_path / entry)
     fe = hoarder.hash_archive.FileEntry(
         path,
         size,
@@ -33,8 +29,27 @@ for entry in os.walk(test_file_path):
         hash,
         hoarder.hash_archive.Algo.CRC32
     )
+    return fe
+
+dummy_dir_entries = []
+
+test_file_path = ap / ".." / "test_files" / "files"
+
+for entry in os.walk(test_file_path):
+    fe = create_test_file_entry(test_file_path, entry)
     dummy_dir_entries.append(fe)
 
 for d in dummy_dir_entries:
     print(d.pretty_print())
     print("\n")
+
+hnf_file_path = ap / ".." / "test_files" / "hnf"
+
+hnf_file_entries = []
+
+for entry in os.walk(hnf_file_path):
+    create_test_file_entry(
+    hnf_file_entries.append
+
+
+
