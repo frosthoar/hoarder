f54c20eee297d67dfc2cf800204f23167e4c82ae
frosthoar <197470064+frosthoar@users.noreply.github.com>
2025-07-20 04:54:36 +0200

    add further tests

--- tests/test_repository.py
+++ tests/test_repository.py
@@ -32,7 +32,16 @@ def test_repository(create_test_dir):
 
         assert repr(saved_sfv_file) == repr(retrieved_sfv_file)
 
-    for rar_fn, password in [("v4_split_headers_encrypted.rar", "password")]:
+    for rar_fn, password in [
+        ("v4_split_headers_encrypted.rar", "password"),
+        ("v4_split_headers_unencrypted.rar", None),
+        ("v4_unencrypted.rar", None),
+        ("v4_encrypted.rar", "secret"),
+        ("v5_split_headers_encrypted.part01.rar", "ninja"),
+        ("v5_split_headers_unencrypted.part01.rar", None),
+        ("v5_headers_encrypted.rar", "dragon"),
+        ("v5_headers_unencrypted.rar", None),
+    ]:
         logger.debug(rar_fn)
         saved_rar_file = hoarder.RarArchive.from_path(
             pathlib.Path("./test_files/rar/") / rar_fn,
