90c4e0140ff35ab8db74975fa5a7be7719f5db44
frosthoar <197470064+frosthoar@users.noreply.github.com>
2025-07-23 23:34:46 +0200

    split test

--- tests/test_repository.py
+++ tests/test_repository.py
@@ -1,23 +1,21 @@
 import logging
 import pathlib
 
-import hoarder
 import pytest
+
+import hoarder
 
 logger = logging.getLogger()
 
 
 @pytest.fixture(scope="session")
-def create_test_dir(tmpdir_factory):
+def create_test_repo(tmpdir_factory):
     p = tmpdir_factory.mktemp("db")
-    print(type(p))
-    return p
+    ha_repo = hoarder.HashArchiveRepository(pathlib.Path(p / "hoarder.db"))
+    return ha_repo
 
 
-def test_repository(create_test_dir):
-    ha_repo = hoarder.HashArchiveRepository(
-        pathlib.Path(create_test_dir / "hoarder.db")
-    )
+def test_sfv_repositories(create_test_repo):
     for sfv_fn in [
         "files.sfv",
     ]:
@@ -27,11 +25,13 @@ def test_repository(create_test_dir):
 
         saved_sfv_file = hoarder.SfvArchive.from_path(pathlib.Path(p))
         saved_sfv_file_path = saved_sfv_file.path
-        ha_repo.save(saved_sfv_file)
-        retrieved_sfv_file = ha_repo.load(saved_sfv_file_path)
+        create_test_repo.save(saved_sfv_file)
+        retrieved_sfv_file = create_test_repo.load(saved_sfv_file_path)
 
         assert repr(saved_sfv_file) == repr(retrieved_sfv_file)
 
+
+def test_rar_repositories(create_test_repo):
     for rar_fn, password in [
         ("v4_split_headers_encrypted.rar", "password"),
         ("v4_split_headers_unencrypted.rar", None),
@@ -48,11 +48,13 @@ def test_repository(create_test_dir):
             password=password,
         )
         saved_rar_file_path = saved_rar_file.path
-        ha_repo.save(saved_rar_file)
-        retrieved_rar_file = ha_repo.load(saved_rar_file_path)
+        create_test_repo.save(saved_rar_file)
+        retrieved_rar_file = create_test_repo.load(saved_rar_file_path)
 
         assert repr(saved_rar_file) == repr(retrieved_rar_file)
 
+
+def test_hnf_repositories(create_test_repo):
     for hnf_fn in [
         "[ABC] 05. Lowercase and Brackets [x265][1080p][8714c76f].mkv",
         "[Dummy] Uppercase and Brackets Ep13v2[Puresuhoruda][6519E6CF].mkv",
@@ -63,7 +65,7 @@ def test_repository(create_test_dir):
             pathlib.Path("./test_files/hnf/") / hnf_fn
         )
         saved_hnf_file_path = saved_hnf_file.path
-        ha_repo.save(saved_hnf_file)
-        retrieved_hnf_file = ha_repo.load(saved_hnf_file_path)
+        create_test_repo.save(saved_hnf_file)
+        retrieved_hnf_file = create_test_repo.load(saved_hnf_file_path)
 
     assert repr(saved_hnf_file) == repr(retrieved_hnf_file)
