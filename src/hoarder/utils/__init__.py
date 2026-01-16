26cdc1b74bcbe641f8ab1222174586a38cb38dc5
frosthoar <197470064+frosthoar@users.noreply.github.com>
2026-01-16 15:13:11 +0100

    linting

--- src/hoarder/utils/__init__.py
+++ src/hoarder/utils/__init__.py
@@ -1,12 +1,7 @@
 from . import db_schema, db_utils, path_utils, presentation, shared, sql3_fk
 from .db_utils import now_str
 from .path_utils import PathType, determine_path_type
-from .presentation import (
-    Presentable,
-    PresentationSpec,
-    ScalarValue,
-    TableFormatter,
-)
+from .presentation import Presentable, PresentationSpec, ScalarValue, TableFormatter
 from .shared import SEVENZIP, config
 from .sql3_fk import Sqlite3FK
 
